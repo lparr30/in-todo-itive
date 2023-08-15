e10f215b3e4ce03def4723b90eecfd169af8f655
Liza Parr <lizajparr@gmail.com>
2023-08-15 00:44:40 -0400

    adds login form fields

--- src/components/Login/Dot.jsx
+++
@@ -1,7 +0,0 @@
-import './Login.css'
-
-const Dot = () => {return(
-    <div id='dot'></div>
-)}
-
-export default Dot;
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,25 +1,45 @@
-import './Login.css'
-import squiggle from '../../assets/squiggle.png'
-import Dot from './Dot'
+import "./Login.css";
+import squiggle from "../../assets/squiggle.png";
+// https://stock.adobe.com/images/squiggly-line/538587269?asset_id=538587269
 
-const Login = () => {return(
-    <div className="login">
-        <div className='login-welcome'>
-            {/* <h1>In<i>todo</i>itive</h1> */}
-            <div id='in-todo-itive'>
-                <h1>In<span className='dot'>•</span><i>todo</i><span className='dot'>•</span>itive</h1>
-                {/* <div id='todo'>
-                    <Dot />
-                    <h1><i>todo</i></h1>
-                    <Dot />
-                </div>
-                <h1>itive</h1> */}
-            </div>
-            <h3>An app designed with <i>you</i> in mind.</h3>
-            <h3>Carpe Diem</h3>
+const Login = () => {
+  function test() {
+    console.log("i got clicked!");
+  }
+
+  return (
+    <div className="login-container">
+      <div className="login-welcome">
+        <div id="in-todo-itive">
+          <h1>
+            In<span className="dot">•</span>
+            <i>todo</i>
+            <span className="dot">•</span>itive
+          </h1>
         </div>
-        <img id='squiggle' src={squiggle} alt="squiggle" />
+        <h3>
+          An app designed with <i>you</i> in mind.
+        </h3>
+      </div>
+      <img id="squiggle" src={squiggle} alt="squiggle" />
+      <div id="login-field">
+        <input id="username" type="text" placeholder="username" />
+        <input id="password" type="text" placeholder="password" />
+        <input
+          id="login-submit"
+          type="submit"
+          value="Submit"
+          onClick={test}
+        ></input>
+        <p id="account">
+          Don't have an account yet?{" "}
+          <a id="sign-up" href="https://www.google.com">
+            Sign up!
+          </a>
+        </p>
+      </div>
     </div>
-)}
+  );
+};
 
 export default Login;
