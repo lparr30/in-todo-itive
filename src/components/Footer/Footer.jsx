60f2df5303a075f946128e9c6eb645496405ca5d
Liza Parr <lizajparr@gmail.com>
2023-08-15 00:54:46 -0400

    adds a footer, still need to fix vertical scrolling

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,7 @@
+const Footer = () => {return(
+    <div>
+        <p style={{color: 'rgb(220, 139, 64)', textAlign: 'center', margin: '0 0 0.5em 0'}}>&copy; Liza Parr 2023</p>
+    </div>
+)}
+
+export default Footer;
