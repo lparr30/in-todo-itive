8ae06c362e051c688f19dde86c759208a1497be1
Liza Parr <lizajparr@gmail.com>
2023-08-16 11:29:03 -0400

    safety

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {return(
     <div>
-        <p style={{color: 'rgb(220, 139, 64)', textAlign: 'center', margin: '0 0 0.5em 0'}}>&copy; Liza Parr 2023</p>
+        <p style={{color: 'rgb(236, 132, 100)', textAlign: 'center', margin: '0 0 0.5em 0'}}>&copy; Liza Parr 2023</p>
     </div>
 )}
 
