7dc37e1c2b0775c48f0dfaa02d91e78b729ab225
Liza Parr <lizajparr@gmail.com>
2023-08-17 20:36:56 -0400

    adds basic heading

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import "./App.css";
-// import { styled } from "styled-components";
+import { styled } from "styled-components";
 import Header from "./components/Header";
 import ChangeView from "./components/ChangeView";
 import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer";
 
-// const Test = styled.div`
-//   width: 100vw;
-//   height: 100vh;
-//   background-color: red;
-// `
+const Test = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: red;
+`
 
 function App() {
   return (
@@ -18,7 +18,7 @@ function App() {
       <Header />
       <ChangeView />
       <Outlet />
-      {/* <Test /> */}
+      <Test />
       <Footer />
     </div>
     </>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,45 @@
 import { styled } from "styled-components";
 
-const HeaderName = styled.h1`
-    color: pink;
+const HeaderContainer = styled.div`
+    height: 7.5vh;
+    position: sticky;
+    top: 0;
+    display: flex;
+    align-items: baseline;
+    // justify-content: space-evenly;
 `
 
-const Header = () => {return(
-    <>
-        <HeaderName>
-            In-todo-itive
-        </HeaderName>
-    </>
-)}
+const Intodoitive = styled.h1`
+  color: rgb(236, 132, 100);
+  margin-left: 5vw;
+`;
+
+const Dot = styled.span`
+  font-size: 0.6em;
+  margin: 0 0.2em;
+`;
+
+const ViewName = styled.h2`
+    color: rgb(180, 220, 132);
+    font-style: italic;
+    margin-left: 20vw;
+`
+
+const Header = () => {
+  return (
+    <HeaderContainer>
+      <Intodoitive>
+        In
+        <Dot>•</Dot>
+        <i>todo</i>
+        <Dot>•</Dot>
+        itive
+      </Intodoitive>
+      <ViewName>
+        Name
+      </ViewName>
+    </HeaderContainer>
+  );
+};
 
 export default Header;
