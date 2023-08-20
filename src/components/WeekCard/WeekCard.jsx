7c87cfae035c307593798808839b758401d33ec0
Liza Parr <lizajparr@gmail.com>
2023-08-20 00:59:10 -0400

    dates now read weekday, date, month

--- src/components/WeekCard/WeekCard.jsx
+++ src/components/WeekCard/WeekCard.jsx
@@ -1,25 +1,23 @@
 import { styled } from "styled-components";
 import WeekDate from "./WeekDate";
 import WeekTasks from "./WeekTasks";
-// import colors from "../../assets/colors";
 
 const TaskCard = styled.div`
   width: 88vw;
-  height: 80px;
+  height: 100px;
   display: flex;
   align-items: flex-start;
-  // background-color: rgb(252, 220, 84);
   background-color: ${(props) => props.$containerColor};
   border-radius: 20px;
   margin: 0.5em 0;
 `;
 
 
-const WeekCard = ({ color, date, weekday }) => {
+const WeekCard = ({ color, weekday, date, month }) => {
 
   return (
     <TaskCard $containerColor={color} >
-      <WeekDate date={date} weekday={weekday}/>
+      <WeekDate date={date} weekday={weekday} month={month}/>
       <WeekTasks />
     </TaskCard>
   );
--- src/pages/WeekView/WeekView.jsx
+++ src/pages/WeekView/WeekView.jsx
@@ -12,7 +12,8 @@ const today = new Date();
 let currentDate = today.getDate();
 let currentWeekday = today.getDay();
 const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-let date, weekday;
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+let date, weekday, month;
 
 
 const WeekView = () => {
@@ -20,15 +21,18 @@ const WeekView = () => {
     <Layout>
       {Array.from({ length: 14 }).map((_, index) => {
         const colorIndex = index % colors.length; //colorIndex = the remainder of index (0,1,2,3...13) / 4 (colors.length)
+        
         date = currentDate + index;
         weekday = weekdays[currentWeekday + index%7]
+        month = months[today.getMonth()];
 
         return (
           <WeekCard
             key={index}
             color={colors[colorIndex]}
-            date={date}
             weekday={weekday}
+            date={date}
+            month={month}
           />
         );
       })}
