9b18109c322bc5b4c6f1a3139c5c04cb3962c443
Liza Parr <lizajparr@gmail.com>
2023-08-20 14:40:10 -0400

    dates are now dependent on the month and the number of days in that month

--- src/pages/WeekView/WeekView.jsx
+++ src/pages/WeekView/WeekView.jsx
@@ -11,20 +11,59 @@ const Layout = styled.div`
 const today = new Date();
 let currentDate = today.getDate();
 let currentWeekday = today.getDay();
-const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+let currentMonth = today.getMonth();
+const weekdays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 let date, weekday, month;
-
 
 const WeekView = () => {
   return (
     <Layout>
       {Array.from({ length: 14 }).map((_, index) => {
         const colorIndex = index % colors.length; //colorIndex = the remainder of index (0,1,2,3...13) / 4 (colors.length)
-        
+
+        weekday = weekdays[currentWeekday + (index % 7)];
+
+        month = months[currentMonth];
+
         date = currentDate + index;
-        weekday = weekdays[currentWeekday + index%7]
-        month = months[today.getMonth()];
+        if (currentMonth === 2) {
+          date > 28 && (date = date % 28);
+        } else if (
+          currentMonth === 4 ||
+          currentMonth === 6 ||
+          currentMonth === 9 ||
+          currentMonth === 11
+        ) {
+          date > 30 && (date = date % 30);
+        } else {
+          date > 31 && (date = date % 31);
+        }
+
+        // if (date >= 1) {
+        //   month = months[currentMonth + 1];
+        // }
 
         return (
           <WeekCard
